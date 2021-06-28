67aafd7856eefc8d1ec6f32ddb2524faa3287a7e
mgbarois <mgbarois@gmail.com>
2021-06-28 18:22:37 +0200

    cleanup, integrated react hooks

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -5,7 +5,7 @@ import React from "react";
 // "Render whatever is inside of you"
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: "scroll", border: "1px solid black", height: "400px" }}>
+        <div style={{ overflow: "scroll", height: "100vh" }}>
             {/* In JSX you can add style with double curly brackets.
             It's a Javascript expression, within which we are returning an object that can have CSS styles */}
             {props.children}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Scroll from "../components/Scroll";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import ErrorBoundary from '../components/ErrorBoundary';
+import ErrorBoundary from './ErrorBoundary';
 
 // import { robots } from './robots'; // needs to be destructured because not default export
 import "./App.css";
 //In order to use state, we have to use classes
 
-class App extends Component {
-  constructor() {
-    super() // necessary to call the constructor of component first
-    this.state = {
-      robots: [],
-      searchField: ""
-    }
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
+
+
+  useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/users") // fetch is a method on the Window object.
+        // It comes with all browsers now: it helps us make requests to servers 
+        // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+        .then(response => response.json())
+        .then(users => {
+          console.log(users);
+          setRobots(users)});
+  }, []); // Add an empty array to simulate only ComponentDidMount (and not ComponentDidUpdate)
+
+  const onSearchChange = (event) => { // When you create your own methods on a component, you have to use arrow function syntax
+   setSearchField(event.target.value);
   }
 
-  componentDidMount() { // No need for arrow function syntax, because function are part of React
-    fetch("https://jsonplaceholder.typicode.com/users") // fetch is a method on the Window object.
-      // It comes with all browsers now: it helps us make requests to servers 
-      // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
-  }
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  });
 
-  onSearchChange = (event) => { // When you create your own methods on a component, you have to use arrow function syntax
-    this.setState({ searchField: event.target.value })
-    // console.log(this.state.searchField);
-  }
-  render() { // react components  always need a render() function
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-
-    return (!robots.length) ?  // or :if (robots.length === 0) ... else.. -> 
+    return !robots.length ?  // or :if (robots.length === 0) ... else.. -> 
       <h1>Loading...</h1> :
       (
         <div className="tc">
           <h1 className="f1">Robofriends</h1>
-          <SearchBox searchChange={this.onSearchChange} /> {/* have to use "this." because we're inside an object (App)*/}
+          <SearchBox searchChange={onSearchChange} /> {/* have to use "this." because we're inside an object (App)*/}
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots} /> {/* get it from the state instead - which contains the filtered robots list*/}
@@ -49,7 +45,10 @@ class App extends Component {
         </div>
       ); //parentheses necessary if you're writing more than one jsx element
   }
-}
 
 export default App;
 //default is used when you're only returning one component.
+
+
+
+
